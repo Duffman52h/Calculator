62adafa3e4cfc19143b4ce829af9d30638f4b6f5
Duffman52h <85713107+Duffman52h@users.noreply.github.com>
2023-06-18 10:24:45 -0400

    Add files via upload

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import "./Button.css";
+import React, { useState } from "react";
+
+const Button = ({ className, value, onClick }) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+
+
+export default Button;
